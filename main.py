a2cff8029f684dde884f8a3c9188c5f0bbb1b0da
JulianChu <yulang.chu@gmail.com>
2018-08-09 18:54:39 +0200

    add calc variance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import glob
 import os
-# PIV　data sorting
 
 
 def getIndexedElementFromFile(filename, indexOfElement=4):
@@ -15,13 +14,24 @@ def getIndexedElementFromFile(filename, indexOfElement=4):
 
 
 def getAvgExcludeZero(nums):
-    target = [num for num in nums if num != 0]
+    target = [float(num) for num in nums if num != '0']
     print(target)
     if len(target) == 0:
         avg = 0
     else:
-        avg = float(target[0]) / len(target)
+        avg = sum(target) / len(target)
     return avg
+
+
+def getVarExcludeZero(nums):
+    target = [float(num) for num in nums if num != '0']
+    print(target)
+    if len(target) == 0:
+        var = 0
+    else:
+        avg = sum(target) / len(target)
+        var = sum((x-avg)**2 for x in target) / len(target)
+    return var
 
 
 files = glob.glob("*_PIV3_disp.txt")
@@ -33,15 +43,17 @@ for file in files:
     elements = getIndexedElementFromFile(file, 4)
     total = sum(float(e) for e in elements)
     avg = getAvgExcludeZero(elements)
+    var = getVarExcludeZero(elements)
 
-    result.append([total, avg])
+    result.append([total, avg, var])
 
-    print(file + ":" + str(total) + " " + str(avg))
+    print(file + ":" + str(total) + " " + str(avg) +
+          " " + str(var) + " ")
 
 
 with open('result.txt', "w+") as newTxt:
     newTxt.write("sum avg variance \n")
     for e in result:
-        newTxt.write(str(e[0])+' '+str(e[1])+'\n')
+        newTxt.write(str(e[0])+' '+str(e[1]) + ' ' + str(e[2])+'\n')
 
 input("Press Enter to continue...")
